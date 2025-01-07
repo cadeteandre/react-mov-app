47b2b991b7a4b272b8a8745d4ac27bb22b9a8e2e
Andre Cadete <andrecadete70@gmail.com>
2025-01-07 14:59:37 +0100

    setting up detail page and fetch by id

---
+++ src/utils/fetches/getMoviesByID.ts
@@ -0,0 +1,16 @@
+import { IMovieByID } from "../../../interfaces/IMovieByID";
+import { BASE_URL } from "../../App";
+import { API_KEY } from "./getAllGenres";
+
+const getMoviesByID = async (setFunc: React.Dispatch<React.SetStateAction<IMovieByID | null>>, movieID: string) => {
+    console.log('test');
+    try {
+        const response = await fetch(`${BASE_URL}movie/${movieID}?api_key=${API_KEY}`);
+        const data: IMovieByID = await response.json();        
+        setFunc(data);
+    } catch (error) {
+        console.error('Error fetching movie data', error);
+    }
+};
+
+export default getMoviesByID;
