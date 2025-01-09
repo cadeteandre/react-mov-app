0a683b6e01228ac5d6f7c914c549795cdb4f2c0e
Lamia Rizk <lamiarizk@gmx.de>
2025-01-09 09:11:51 +0100

    css

--- src/pages/intro/Intro.tsx
+++ src/pages/intro/Intro.tsx
@@ -5,14 +5,16 @@ import image1 from '/images/Image.png';
 const Intro: React.FC = () => {
     return (  
         <section className="intro-section">
-        <div className="img-area">
-            <img src={image1} alt="image 1" />
-        </div>
-            <h1>Enjoy Your Movie Watch Everywhere</h1>
-            <p>Stream unlimited movies and TV shows on your phone, tablet, laptop, and TV.</p>
-            <Link to={'/home'}>
+            <div className="img-area">
+                <img src={image1} alt="image 1" />
+            </div>
+            <article>
+            <h1>Enjoy Your Movie<br/>Watch Everywhere</h1>
+                <p>Stream unlimited movies and TV shows<br/>on your phone, tablet, laptop, and TV.</p>
+                <Link to={'/home'}>
                 <button>Get Started</button>
-            </Link>
+                </Link>
+            </article>
         </section>
     );
 }
